7d745440d6f073ca374b4cd873668e1b311e032b
bobo <65712437+bobo482@users.noreply.github.com>
2021-08-26 17:53:04 +0800

    Delete assets directory

--- assets/js/copy.js
+++
@@ -1,6 +0,0 @@
-console.log(
-
-	'font-family: "微软雅黑", sans-serif;font-size:50px;color:#3AAFFF;-webkit-text-fill-color:#3AAFFF;-webkit-text-stroke: 1px #2390DA;text-shadow: 0px 0px 2px #686868, 0px 1px 1px #ddd, 0px 2px 1px #d6d6d6, 0px 3px 1px #ccc, 0px 4px 1px #c5c5c5, 0px 5px 1px #c1c1c1, 0px 6px 1px #bbb, 0px 7px 1px #777, 0px 8px 3px rgba(100, 100, 100, 0.4), 0px 9px 5px rgba(100, 100, 100, 0.1), 0px 10px 7px rgba(100, 100, 100, 0.15), 0px 11px 9px rgba(100, 100, 100, 0.2), 0px 12px 11px rgba(100, 100, 100, 0.25), 0px 13px 15px rgba(100, 100, 100, 0.3);',
-	"font-size:16px;color:#f3c064;",
-	"font-size:12px;color:#999999;"
-);
